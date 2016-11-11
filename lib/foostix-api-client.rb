9c49e975a9f44fa5f29eaf2db5a47bd5b44ccea1
Stephen Edwards <stephen.edwards@foostix.lu>
2016-11-11 15:16:18 +0100

    some dev.

--- lib/foostix-api-client.rb
+++ lib/foostix-api-client.rb
@@ -2,14 +2,17 @@ require 'rest-client'
 require 'json'
 
 module FoostixApi
-  @options = {}  
+  @options = {}
+  @token = nil
+
   class << self
     attr_accessor :options
   end
   
   private
   class DefaultAction
-    def initialize      
+    @path = nil
+    def self.initialize      
       if !FoostixApi.options.has_key?(:api_key)
         raise "API KEY required"
       end
@@ -18,8 +21,16 @@ module FoostixApi
       end
     end
         
-    def fetch_token
-      RestClient.post(FoostixApi.options[:api_uri]+"/auth", JSON.generate({:api_key => FoostixApi.options[:api_key]}), {content_type: :json, accept: :json})
+    def self.fetch_token
+      if @token.nil? || Time.parse(@token["expires_at"]) < Time.now()
+        response = RestClient.post(FoostixApi.options[:api_uri]+"/auth", JSON.generate({:api_key => FoostixApi.options[:api_key]}), {content_type: :json, accept: :json})                
+        @token = JSON.parse(response.body)
+      end
+      return @token["token"]
+    end
+
+    def self.get(params)
+      RestClient.get(FoostixApi.options[:api_uri]+@path, JSON.generate(params), {content_type: :json, accept: :json})
     end
   end
   
@@ -27,9 +38,7 @@ module FoostixApi
   module Search
     module Geoportail
       class Locationsearch < DefaultAction
-        def initialize
-          super
-        end
+        @path = '/search/geoportail/locationsearch'
       end
     end
   end
